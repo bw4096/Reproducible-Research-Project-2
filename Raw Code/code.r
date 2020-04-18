76b6a99e920bf8f79945b0ba6ee261250c2ebb80
Bryan Willauer <bw4096@yahoo.com>
2020-04-18 16:57:10 -0400

    added code to calculate casualities and damage

--- Raw Code/code.r
+++ Raw Code/code.r
@@ -1,3 +1,24 @@
 
 
 df <- read.csv("repdata_data_StormData.csv.bz2")
+
+library(tidyverse)
+df %>% group_by(EVTYPE) %>%
+        summarize(Casualities = sum(FATALITIES + INJURIES)) %>%
+        arrange(desc(Casualities))
+
+real_damage <- function(number, multiplier){
+        if(multiplier == 'B'){
+                return(number * 1000000000)
+        } else if(multiplier == 'M'){
+                return(number * 1000000)
+        } else if(multiplier == 'K'){
+                return(number * 1000)
+        } else{
+                return(number)
+        }
+}
+
+df$prop_damage <- mapply(real_damage, df$PROPDMG, df$PROPDMGEXP)
+df$crop_damage <- mapply(real_damage,df$CROPDMG,df$CROPDMGEXP)
+df$econ_damage <- df$prop_damage + df$crop_damage
