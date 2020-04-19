e2e362fda900bf6afbd48f52610bacde333db480
Bryan Willauer <bw4096@yahoo.com>
2020-04-19 04:18:46 -0400

    added graph code

--- Raw Code/code.r
+++ Raw Code/code.r
@@ -3,9 +3,45 @@
 df <- read.csv("repdata_data_StormData.csv.bz2")
 
 library(tidyverse)
-df %>% group_by(EVTYPE) %>%
-        summarize(Casualities = sum(FATALITIES + INJURIES)) %>%
-        arrange(desc(Casualities))
+library(lubridate)
+library(scales)
+
+df$year <- year(as.Date(df$BGN_DATE, format = "%m/%d/%Y"))
+df %>% ggplot(aes(year)) +
+        geom_histogram(binwidth = 1, color="Black", fill="lightblue") +
+        ylab("Number of Events") +
+        xlab("Year")
+
+df1 <- df %>% filter(year >= 1995)
+
+
+deaths <- df1 %>% group_by(EVTYPE) %>%
+        summarize(Fatalites = sum(FATALITIES)) %>%
+        arrange(desc(Fatalites)) %>%
+        top_n(10)
+
+deaths %>% ggplot(aes(x = reorder(EVTYPE,Fatalites),Fatalites)) +
+        geom_col(col = "Black", fill = "Red") +
+        coord_flip() +
+        ylab("Total Fatalities") +
+        xlab("Event Type") +
+        ggtitle("Fatalities by Weather Event") +
+        scale_y_continuous(breaks = seq(0,2000,len = 9))
+
+
+injured <- df1 %>% group_by(EVTYPE) %>%
+        summarize(Injuries = sum(INJURIES)) %>%
+        arrange(desc(Injuries)) %>%
+        top_n(10)
+
+injured %>% ggplot(aes(x = reorder(EVTYPE,Injuries),Injuries)) +
+        geom_col(col = "Black", fill = "Yellow") +
+        coord_flip() +
+        ylab("Total Injuries") +
+        xlab("Event Type") +
+        ggtitle("Injuries by Weather Event") +
+        scale_y_continuous(breaks = seq(0,25000,len = 6))
+
 
 real_damage <- function(number, multiplier){
         if(multiplier == 'B'){
@@ -19,10 +55,20 @@ real_damage <- function(number, multiplier){
         }
 }
 
-df$prop_damage <- mapply(real_damage, df$PROPDMG, df$PROPDMGEXP)
-df$crop_damage <- mapply(real_damage,df$CROPDMG,df$CROPDMGEXP)
-df$econ_damage <- df$prop_damage + df$crop_damage
+df1$prop_damage <- mapply(real_damage, df1$PROPDMG, df1$PROPDMGEXP)
+df1$crop_damage <- mapply(real_damage,df1$CROPDMG,df1$CROPDMGEXP)
+df1$econ_damage <- df1$prop_damage + df1$crop_damage
 
-df %>% group_by(EVTYPE) %>% 
+damage <- df1 %>% group_by(EVTYPE) %>% 
         summarize(Total.Damage = sum(econ_damage)) %>% 
-        arrange(desc(Total.Damage))
+        arrange(desc(Total.Damage)) %>%
+        top_n(10)
+
+damage %>% ggplot(aes(x = reorder(EVTYPE,Total.Damage),Total.Damage)) +
+        geom_col(col = "Black", fill = "Orange") +
+        coord_flip() +
+        ylab("Total Damage (Billons of Dollars)") +
+        xlab("Event Type") +
+        ggtitle("Economic Damage by Weather Event") +
+        scale_y_continuous(breaks = seq(0, 150000000000, len = 7),labels = unit_format(unit="", scale = 1e-9))
+
